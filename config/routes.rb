8b5b94d4cd595f477b209398aa4ff872498baeb6
Patrick <patrick.edward.alcindor@gmail.com>
2018-12-30 16:13:15 -0500

    can login

--- config/routes.rb
+++ config/routes.rb
@@ -5,53 +5,6 @@ Rails.application.routes.draw do
   resources :instructors
   resources :cohorts
   root to: 'pages#home'
-
-  
-
-  root 'dashboard#index'
-
-  devise_for :users, controllers: {
-      sessions: 'users/sessions',
-      passwords: 'users/passwords',
-      registrations: 'users/registrations'
-  }
-
-  get 'dashboard/index'
-  get 'dashboard/home'
-
-
-  # resources :instructors do
-  #     member do
-  #         get :delete
-  #     end
-  # end
-
-  # resources :students do
-  #     member do
-  #         get :delete
-  #     end
-  # end
-
-  # resources :courses do
-  #     member do
-  #         get :delete
-  #     end
-  # end
-
-  # resources :course_registrations do
-  #     member do
-  #         get :delete
-  #     end
-  # end
-
-  # resources :cohorts do
-  #     member do
-  #         get :delete
-  #     end
-  # end
-
-
-
-  get "students/new" => 'students#new', :as => :new
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
