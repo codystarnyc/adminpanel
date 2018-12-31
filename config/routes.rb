9463cbe9bacc2e74f6f85eab7ae5de2154a3a50b
Patrick <patrick.edward.alcindor@gmail.com>
2018-12-30 22:47:00 -0500

    added //= require jquery //= require jquery_ujs

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,10 @@ Rails.application.routes.draw do
   resources :instructors
   resources :cohorts
   root to: 'pages#home'
-  devise_for :users
+ 
+
+  devise_for :users do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
