dc098c9e708b0430b7f698ae0197dba7d4ef09d9
Patrick <patrick.edward.alcindor@gmail.com>
2019-01-02 21:31:37 -0500

    fixing modal

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,11 @@ Rails.application.routes.draw do
   resources :courses
   resources :instructors
   resources :cohorts
-  root to: 'pages#home'
- 
 
+  root to: 'pages#home'
+  get 'dashboard/index' 
+  get 'dashboard/home'
+  get 'students/new' => 'students#new', :as => :new
   devise_for :users do
     get '/users/sign_out' => 'devise/sessions#destroy'
 end
