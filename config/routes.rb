c0cc6cf46eb8afdf7bb8211e9fc5fe134065ff7e
Patrick <patrick.edward.alcindor@gmail.com>
2019-01-10 01:34:43 -0500

    migration

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,13 @@ Rails.application.routes.draw do
   get 'users/show/:id' => 'users#show'
   get 'users/edit/:id' => 'users#edit'
   get 'users/index' => 'users#index'
+
+  devise_for :users do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+    get '/users/edit/:id' => 'devise/sessions#edit'
+    get '/users/show/:id' => 'devise/sessions#show'
+end
+
   resources :instructors do
     member do
         get :delete
@@ -31,14 +38,12 @@ resources :cohorts do
         get :delete
     end
 end
+
   root to: 'pages#home'
   get 'dashboard/index' 
   get 'dashboard/home'
   get 'students/new' => 'students#new', :as => :new
-  devise_for :users do
-    get '/users/sign_out' => 'devise/sessions#destroy'
-    get '/users/edit/:id' => 'devise/sessions#edit'
-    get '/users/show/:id' => 'devise/sessions#show'
-end
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
