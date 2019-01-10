5b4bfa89e82fa94fbb3cd7abc4a56701108eb115
Patrick <patrick.edward.alcindor@gmail.com>
2019-01-10 11:01:11 -0500

    updating sentry

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,9 @@
 class ApplicationController < ActionController::Base
 
     protect_from_forgery with: :exception
+    before_action :authenticate_user!
     before_action :configure_permitted_parameters, if: :devise_controller?
+    before_action :set_raven_context
 
     rescue_from CanCan::AccessDenied do |exception|
       respond_to do |format|
@@ -10,6 +12,15 @@ class ApplicationController < ActionController::Base
         format.js   { head :forbidden, content_type: 'text/html' }
       end
     end
+
+
+    
+    def after_sign_in_path_for(resource)
+        dashboard_index_path
+    end
+
+   
+  
    
    
 protected
@@ -19,6 +30,11 @@ def configure_permitted_parameters
 #   devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:email, :password, :remember_me)} 
   devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:first_name, :last_name, :email, :password,
   :student, :instructor, :admin)} 
+end
+
+def set_raven_context
+  Raven.user_context(id: session[:current_user_id]) # or anything else in session
+  Raven.extra_context(params: params.to_unsafe_h, url: request.url)
 end
 
 end
