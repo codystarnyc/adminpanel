6aa82805a6c51bc82d9cdce9228e9ab1d3944a07
Patrick <patrick.edward.alcindor@gmail.com>
2019-01-11 02:27:13 -0500

    updated csfr

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
-    skip_before_action :verify_authenticity_token
-    protect_from_forgery with: :exception
+  
+    protect_from_forgery with: :null_session
     before_action :authenticate_user!
     before_action :configure_permitted_parameters, if: :devise_controller?
    
